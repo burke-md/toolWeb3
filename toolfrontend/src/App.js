c5b47c4ff115f3d901120763b641cc5ceecc107a
Michael Burke <michael.dburke56@gmail.com>
2022-04-16 16:43:18 -0400

    Connect to Tool NFT contract and read number of minted tokens.

--- toolfrontend/src/App.js
+++ toolfrontend/src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import Web3 from 'web3';
 import { toolABI } from './data/abi';
 import './App.css';
@@ -6,14 +7,31 @@ const web3 = new Web3(Web3.givenProvider);
 const contractAddress = "0x3215f99412a72f9660Aed1D8aE552e5876804693";
 const ToolContract = new web3.eth.Contract(toolABI, contractAddress);
 
+const getTokenCount = async () => {
+  const numTokens = await ToolContract.methods.getNumMintedTokens().call();
+  return numTokens;
+}
+
 function App() {
+  const [numMintedTokens, setNumMintedTokens] = useState(0);
+
+  useEffect(() => {
+    getTokenCount()
+    .then((num) => {
+      setNumMintedTokens(num)
+    })
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <div>
           Tool NFT
-        </p>
+        </div>
       </header>
+      <span>
+        There have been {numMintedTokens} Tool tokens created.
+      </span>
     </div>
   );
 }
